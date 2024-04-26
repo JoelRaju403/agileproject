0b57edf1dce0d0fa178853d53390048562d8f4d6
Andy <andylee4930@gmail.com>
2024-04-26 11:57:53 +0800

    A database is made to holder usernames, emails and hashed passwords. Users can also register an account
    accepted both changes in routes

--- app/routes.py
+++ app/routes.py
@@ -79,7 +79,6 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
-
 @app.route('/create')
 @login_required
 def create():

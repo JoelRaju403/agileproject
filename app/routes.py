1060f2a8a449a101303735c0833758264a0b6dd0
Joel <rajujoel403@gmail.com>
2024-04-28 00:17:32 +0800

    updated the register html file to improve styling and UI

--- app/routes.py
+++ app/routes.py
@@ -31,7 +31,7 @@ def home():
     elif request.referrer == request.url:
         # If the form was submitted without anything, redirect to login page to try again.
         return redirect(url_for('login'))
-    return render_template('home.html', form=form)
+    return render_template('home.html', form=form, page='home')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -49,7 +49,7 @@ def login():
         if not next_page or urlsplit(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Sign In', form=form, page="login")
 
 
 @app.route('/logout')
@@ -75,9 +75,11 @@ def register():
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title='Register', form=form, page='register')
 
 
 @app.route('/create')
+@login_required
 def create():
+  
   return render_template('Create.html')

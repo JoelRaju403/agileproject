6006854c7926c9323099d4011717cda6754207bc
Umar Shaheen <umar.shaheen93@gmail.com>
2024-04-30 15:24:28 +0800

    added database for the flashcards, they are now sent to the server and saved onto there. The beginnings of an explore page has been made

---
+++ app/models.py
@@ -0,0 +1,54 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timezone
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+from flask_login import UserMixin
+from app import login
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+
+class User(UserMixin, db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+                                             unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+   
+    
+    sets = db.relationship('Sets', backref='user', lazy=True)
+    
+
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+
+class Sets(db.Model):
+    userId = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(100), nullable = False)
+    title = db.Column(db.String(100),nullable=False)
+
+    cards = db.relationship('Cards', backref='set', lazy=True)
+
+
+
+class Cards(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    setId=db.Column(db.Integer, db.ForeignKey(Sets.id))
+    question=db.Column(db.String(400), nullable=False)
+    answer=db.Column(db.String(400), nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for, request
 from app import app
-
+from app.forms import LoginForm
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from app.models import Sets
+from app.models import Cards
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from urllib.parse import urlsplit
+from app.forms import RegistrationForm
+from flask import jsonify
 @app.route('/')
 @app.route('/home')
 def home():
@@ -10,7 +22,36 @@ def home():
 def login():
   return render_template('Login.html')
 
-
 @app.route('/create')
 def create():
   return render_template('Create.html')
+
+@app.route('/explore')
+def explore():
+  return render_template('Explore.html')
+
+
+
+@app.route('/save_flashcards', methods=['POST'])
+def save_flashcards():
+  data = request.json
+
+  subject = data.get('subject')
+  title = data.get('title')
+
+  set_obj = Sets(userId=1, subject=subject, title=title)
+  set_id=set_obj.id
+  db.session.add(set_obj)
+
+  flashcards = data.get('flashcards')
+  for flashcard_info in flashcards:
+    question= flashcard_info.get('my_question')
+    answer = flashcard_info.get('my_answer')
+
+    card = Cards(question=question, answer=answer, setId=set_id)
+    db.session.add(card)
+  
+  db.session.commit()
+
+  return jsonify({'message': 'Flashcards saved successfully'}), 200
+

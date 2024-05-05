39a5439983a1459f3e48c5c429cc0d0b8a653b3a
Andy <andylee4930@gmail.com>
2024-05-05 19:13:31 +0800

    Dark mode now works, one issue on the setting page there is a white section when its darkmode that i need help getting rid of.

--- app/static/setting.js
+++ app/static/setting.js
@@ -16,3 +16,37 @@ function deleteAcc(userId) {
 }
 
 }
+
+function darkmode() {
+  const main = document.getElementById('main');
+  main.classList.toggle('darkmode');
+
+  const header = document.getElementById('header');
+  const footer = document.getElementById('footer');
+  header.classList.toggle('darkhfmode');
+  footer.classList.toggle('darkhfmode');
+
+
+  //save the dark mode state local storage
+  let isDarkMode = main.classList.contains('darkmode');
+  localStorage.setItem('darkModeEnabled', isDarkMode);
+  
+ 
+}
+
+function initalDarkMode() {
+  let isDarkMode = localStorage.getItem('darkModeEnabled') == 'true';
+  const darktoggle = document.getElementById('DMtoggle');
+  const header = document.getElementById('header');
+  const footer = document.getElementById('footer');
+  let main = document.getElementById('main');
+  if (isDarkMode) {
+    main.classList.toggle('darkmode');
+    header.classList.toggle('darkhfmode');
+    footer.classList.toggle('darkhfmode');
+    darktoggle.checked = true;
+  }
+}
+
+window.addEventListener('load', initalDarkMode);
+

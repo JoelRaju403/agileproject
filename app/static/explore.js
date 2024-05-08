96fe5d070443739b339f512838e869c79c647df9
Umar Shaheen <umar.shaheen93@gmail.com>
2024-05-08 16:35:48 +0800

    "Made the flashcards learn page"

--- app/static/explore.js
+++ app/static/explore.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
   const searchContainer = document.getElementsByClassName("searchContainer")[0];
   const searchInput = document.getElementById('searchInput');
   const myCardsHeading = document.getElementById('myCardsHeading');
+
+  var myCardDiv = document.getElementsByClassName("myCards");
+  addListeners();
+
+
+
   
     
   searchInput.addEventListener('keyup', function (event) {
@@ -34,6 +40,10 @@ document.addEventListener('DOMContentLoaded', function () {
           myCardsHeading.parentNode.insertBefore(line, myCardsHeading);
         
           result.forEach(divMaker);
+
+          myCardDiv = document.getElementsByClassName("myCards");
+          addListeners();
+
           
         },
         error: function (xhr, status, error) {
@@ -45,34 +55,77 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
 
-    function divMaker(text) {
-      var div = document.createElement("div");
-      var h2_subject = document.createElement("h2");
-      var h3_title = document.createElement("h3");
+  function divMaker(text) {
+    var div = document.createElement("div");
+    var h2_subject = document.createElement("h2");
+    var h3_title = document.createElement("h3");
       
-      div.className = 'myCards';
+    div.className = 'myCards';
+    div.setAttribute('id', text.id);
+    div.setAttribute('onclick','sendID()');
     
-      h2_subject.innerHTML = text.subject;
+    h2_subject.innerHTML = text.subject;
     
-      h3_title.innerHTML = text.title;
+    h3_title.innerHTML = text.title;
     
     
-      div.appendChild(h2_subject);
-      div.appendChild(h3_title);
+    div.appendChild(h2_subject);
+    div.appendChild(h3_title);
       
     
-      searchContainer.appendChild(div);
+    searchContainer.appendChild(div);
     
-    
-    
-    }
+  }
 
-    function clearBox(className) { 
-      while(className.firstChild) { 
-          className.removeChild(className.firstChild); 
-      } 
-    }
-});
+  function clearBox(className) { 
+    while(className.firstChild) { 
+      className.removeChild(className.firstChild); 
+    } 
+  }
+
   
+
+  function addListeners(){
+    for(let i = 0; i < myCardDiv.length; i++){
+      myCardDiv[i].addEventListener('click',function (event){
+        var id = myCardDiv[i].getAttribute("id");
+        alert(id);
+        console.log(id);
+
+        const toSend = JSON.stringify({id: id});
+        console.log(toSend);
+
+        $.ajax({
+          url: '/sendId',
+          method: 'POST',
+          contentType: 'application/json',
+          data: toSend,
+          success: function(){
+            console.log('success')
+            window.location.href='/learn';
+          },
+
+
+          error: function (xhr, status, error){
+            console.error('Error retrieving Cards:', error);
+          }
+        });
+    
+      
+
+
+
+
+      });
+    }
+ }
+
+    
+
+    
+
+    
+});
+
 
 

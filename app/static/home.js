fdc282567793c1a13a358e41071b18c37b0d327e
Andy <andylee4930@gmail.com>
2024-05-19 09:50:32 +0800

    FIxed up get started button on home page when user is logged in. Takes them to explore instead of log in page.

--- app/static/home.js
+++ app/static/home.js
@@ -1,6 +1,5 @@
-
-
   const modal = document.getElementById('login-modal');
+  
 
 
  // const loginBtn = document.getElementById('login-btn');
@@ -12,6 +11,14 @@
 
   const page = document.documentElement;
 
+  document.addEventListener('DOMContentLoaded', function() {
+    var getStartedExplore = document.getElementById('explore-btn1');
+    
+      getStartedExplore.addEventListener('click', function() {
+        window.location.href = '/explore';
+      });
+    }
+  );
 
   // When the user clicks the 'get started' button, open the modal 
   loginBtn1.addEventListener('click', function(event) {
@@ -20,6 +27,7 @@
     modal.classList.add('slide-up'); // Add slide-up class
   });
 
+ 
 
   // When the user clicks on <span> (x), close the modal
   closeBtn.onclick = function() {

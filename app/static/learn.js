27b81ee2a5b810ae5e6f46797b49527dfb5cabd8
Umar Shaheen <umar.shaheen93@gmail.com>
2024-05-19 09:31:54 +0800

    reduced animation time in javascript to stop multiple cards forming

--- app/static/learn.js
+++ app/static/learn.js
@@ -2,16 +2,16 @@ var cards = $(".card");
 var cardIndex = 0;
 
 $("#next").click(function() {
-    $(cards[cardIndex]).fadeOut(400, function() {
+    $(cards[cardIndex]).fadeOut(200, function() {
         cardIndex = (cardIndex + 1) % cards.length;
-        $(cards[cardIndex]).fadeIn(400);
+        $(cards[cardIndex]).fadeIn(40);
     });
 });
 
 $("#prev").click(function() {
-    $(cards[cardIndex]).fadeOut(400, function() {
+    $(cards[cardIndex]).fadeOut(200, function() {
         cardIndex = (cardIndex - 1 + cards.length) % cards.length;
-        $(cards[cardIndex]).fadeIn(400);
+        $(cards[cardIndex]).fadeIn(200);
     });
 });
 

6d55c4da0c8a6880b2e66927db8e469d1e62c9f9
Joel <rajujoel403@gmail.com>
2024-05-19 09:40:35 +0800

    updated learn page functionality

--- app/static/learn.js
+++ app/static/learn.js
@@ -2,19 +2,45 @@ var cards = $(".card");
 var cardIndex = 0;
 
 $("#next").click(function() {
-    $(cards[cardIndex]).fadeOut(400, function() {
+    $(cards[cardIndex]).fadeOut(1, function() {
         cardIndex = (cardIndex + 1) % cards.length;
-        $(cards[cardIndex]).fadeIn(400);
+        $(cards[cardIndex]).fadeIn(1);
     });
 });
 
 $("#prev").click(function() {
-    $(cards[cardIndex]).fadeOut(400, function() {
+    $(cards[cardIndex]).fadeOut(1, function() {
         cardIndex = (cardIndex - 1 + cards.length) % cards.length;
-        $(cards[cardIndex]).fadeIn(400);
+        $(cards[cardIndex]).fadeIn(1);
     });
 });
 
 $(".card").click(function() {
     $(this).find(".answer").toggle();
+});
+
+
+document.addEventListener('keydown', function (event) {
+    const key = event.key.toUpperCase().trim();
+    console.log(key);
+    if(key =="ARROWRIGHT"){
+        
+            $(cards[cardIndex]).fadeOut(1, function() {
+                cardIndex = (cardIndex + 1) % cards.length;
+                $(cards[cardIndex]).fadeIn(1);
+            });
+      
+    }
+
+    if(key =="ARROWLEFT"){
+        
+            $(cards[cardIndex]).fadeOut(1, function() {
+                cardIndex = (cardIndex + 1) % cards.length;
+                $(cards[cardIndex]).fadeIn(1);
+        });
+    }
+
+    if(key ==" "){
+            $(this).find(".answer").toggle();
+}
 });
